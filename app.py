b9f722cc03b50009dae4897db8c3c10da2092a2c
Shubham Prajapati <shubhamtech1104@gmail.com>
2026-09-25 16:46:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import replicate
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
@@ -7,9 +8,11 @@ UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+# Yeh automatically Render ya aapke computer ke Environment Variable se token utha lega
+# (Local testing ke liye aap terminal me export kar sakte hain, ya Render dashboard me add karenge)
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    uploaded_image_url = None
     views = None
     
     if request.method == 'POST':
@@ -24,12 +27,10 @@ def index():
             filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
             
-            # User ki upload ki gayi shirt ka exact URL
             uploaded_image_url = url_for('static', filename=f'uploads/{filename}')
             
-            # Wahi uploaded shirt ab saare 6 views mein dynamically dikhegi
             views = [
-                {'title': '1. Collar & Brand Tag View', 'url': uploaded_image_url},
+                {'title': '1. Collar & Back View', 'url': uploaded_image_url},
                 {'title': '2. Pocket & Button View', 'url': uploaded_image_url},
                 {'title': '3. Fabric Texture Close-up', 'url': uploaded_image_url},
                 {'title': '4. Model Sitting View', 'url': uploaded_image_url},
@@ -37,7 +38,7 @@ def index():
                 {'title': '6. Studio Portrait View', 'url': uploaded_image_url}
             ]
             
-    return render_template('index.html', views=views, uploaded_image=uploaded_image_url)
+    return render_template('index.html', views=views)
 
 if __name__ == '__main__':
     app.run(debug=True)
